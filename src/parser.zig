7f92e756a0cf03f6b208ff91e42c4b24c5963c13
lytong660-cell <lytong660-cell@users.noreply.github.com>
2026-08-24 06:39:19 +0000

    Auto save: 2026-08-24 06:39:19

--- src/parser.zig
+++ src/parser.zig
@@ -541,7 +541,7 @@ pub const Parser = struct {
     fn parseSystemTagName(self: *Parser) ![]const u8 {
         const tok = self.current();
         if (tok == .keyword) {
-            const name = keywordText(tok.keyword);
+            const name = lexer.Token.keywordText(tok.keyword);
             _ = self.advance();
             return name;
         }
