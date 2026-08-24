ed0b8f12f625bced3bdb0cbd3bdacda6ccea1af0
lytong660-cell <lytong660-cell@users.noreply.github.com>
2026-08-24 06:37:23 +0000

    Auto save: 2026-08-24 06:37:23

--- src/parser.zig
+++ src/parser.zig
@@ -153,7 +153,7 @@ pub const Parser = struct {
             .args = try self.allocator.dupe(ast.Expr, &[_]ast.Expr{expr.*}),
         };
         const e = try self.allocator.create(ast.Expr);
-        e.* = .{ .call = call.* };
+        e.* = .{ .call = call };
         return ast.Statement{ .expr = e };
     }
 
@@ -495,7 +495,7 @@ pub const Parser = struct {
             .args = try self.allocator.dupe(ast.Expr, &[_]ast.Expr{expr.*}),
         };
         const e = try self.allocator.create(ast.Expr);
-        e.* = .{ .call = call.* };
+        e.* = .{ .call = call };
         return ast.Statement{ .expr = e };
     }
 
