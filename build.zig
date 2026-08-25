9c3c6e6acca6821b16153bd3b8bfc916dd350888
lytong660-colab_Bot <lytong660-colab_bot@users.noreply.github.com>
2026-08-25 03:56:56 +0000

    Colab Bot Auto save: 2026-08-25 03:56:56

--- build.zig
+++ build.zig
@@ -22,4 +22,22 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the compiler");
     run_step.dependOn(&run_cmd.step);
+
+    const test_step = b.step("test", "Run all tests");
+    const files = [_][]const u8{
+        "src/test_3600.zig",
+        "src/test_harness.zig",
+        "src/test_lexer_extended.zig",
+        "src/test_parser_extended.zig",
+    };
+    inline for (files) |file| {
+        const test_exe = b.addTest(.{
+            .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = file } },
+            .target = target,
+            .optimize = optimize,
+        });
+        test_exe.addModule("root", exe.root_module);
+        const test_run = b.addRunArtifact(test_exe);
+        test_step.dependOn(&test_run.step);
+    }
 }
