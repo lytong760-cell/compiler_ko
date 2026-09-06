4d91157966c659f51ca9de547a1554bae342f368
lytong660-cell <lytong660-cell@users.noreply.github.com>
2026-09-06 08:24:04 +0000

    Auto save: 2026-09-06 08:24:04

--- src/value.zig
+++ src/value.zig
@@ -467,8 +467,8 @@ pub const ClassDef = struct {
         }
         self.public_methods.deinit();
         if (self.private_scope) |ps| {
-            var piter = ps.variables.iterator();
-            while (piter.next()) |entry| {
+            var priv_iter = ps.variables.iterator();
+            while (priv_iter.next()) |entry| {
                 ps.allocator.free(entry.key_ptr.*);
                 entry.value_ptr.*.deinit(ps.allocator);
             }
@@ -478,8 +478,8 @@ pub const ClassDef = struct {
             self.allocator.destroy(ps);
         }
         if (self.public_scope) |ps| {
-            var ppiter = ps.variables.iterator();
-            while (ppiter.next()) |entry| {
+            var pub_iter = ps.variables.iterator();
+            while (pub_iter.next()) |entry| {
                 ps.allocator.free(entry.key_ptr.*);
                 entry.value_ptr.*.deinit(ps.allocator);
             }
